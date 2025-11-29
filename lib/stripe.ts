c84665fe651eb50e3ee065a25be13c2316286192
L1ttl3N30 <87819388+L1ttl3N30@users.noreply.github.com>
2025-11-29 16:47:10 +0700

    Update stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,1 +1,9 @@
+import Stripe from "stripe";
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: "2024-06-20"
+});
+
+export const STRIPE_WEEKLY_PRICE_ID = process.env.STRIPE_WEEKLY_PRICE_ID || "";
+export const STRIPE_MONTHLY_PRICE_ID = process.env.STRIPE_MONTHLY_PRICE_ID || "";
 
