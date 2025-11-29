05f728c1c540814278afaae9b9d7ca8b9f4302d5
L1ttl3N30 <87819388+L1ttl3N30@users.noreply.github.com>
2025-11-29 16:47:47 +0700

    Update access.ts

--- lib/access.ts
+++ lib/access.ts
@@ -1,1 +1,23 @@
+import { prisma } from "./prisma";
+
+export async function userHasAccessToTest(userId: string, testId: string): Promise<boolean> {
+  const now = new Date();
+  const purchases = await prisma.purchase.findMany({
+    where: {
+      userId,
+      status: "ACTIVE",
+      OR: [
+        { testId },
+        {
+          type: "SUBSCRIPTION",
+          expiresAt: {
+            gt: now
+          }
+        }
+      ]
+    }
+  });
+
+  return purchases.length > 0;
+}
 
