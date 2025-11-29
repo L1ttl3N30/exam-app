aeee25eba052233be84549bbd10365ae32707c95
L1ttl3N30 <87819388+L1ttl3N30@users.noreply.github.com>
2025-11-29 16:45:13 +0700

    Update next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,1 +1,9 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+declare module "*.svg" {
+  import * as React from "react";
+  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
+}
 
