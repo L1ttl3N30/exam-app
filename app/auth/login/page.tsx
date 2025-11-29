d2c01aafbd4341c194f83f633051e7c5eab335b6
L1ttl3N30 <87819388+L1ttl3N30@users.noreply.github.com>
2025-11-29 17:07:27 +0700

    Update page.tsx

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,1 +1,10 @@
+import { AuthForm } from "@/components/AuthForm";
+
+export default function LoginPage() {
+  return (
+    <div className="py-12">
+      <AuthForm mode="login" />
+    </div>
+  );
+}
 
